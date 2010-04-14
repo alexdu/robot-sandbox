88f7e9b4ec8068411ad5c8e71e7eb459eb8933a5
unknown <Alex@.(none)>
2010-04-14 11:02:22 +0300

    Experiments in setup script

--- sim-files/setup.py
+++ sim-files/setup.py
@@ -2,6 +2,57 @@ from distutils.core import setup
 from os.path import join
 import py2exe
 
+import sys, os
+import zipfile
+import shutil
+
+def rmrf(folder, exc = False):
+    if sys.platform == "win32":
+        rm = "rmdir /s /q "
+    else:
+        rm = "rm -rf "
+
+    print rm + folder
+    ret = os.system(rm + folder)
+    if exc:
+        if ret != 0: 
+            raise Exception, "error deleting " + folder
+
+def unzip(file, dir):
+    zfobj = zipfile.ZipFile(file)
+    mainfolder = zfobj.namelist()[0][:-1]
+    for name in zfobj.namelist():
+        print name
+        fullname = os.path.join(dir, name)
+        d = os.path.dirname(fullname)
+        if d != "":
+            if not os.path.isdir(d):
+                os.makedirs(d)
+        if not os.path.isdir(fullname):
+            outfile = open(fullname, 'wb')
+            outfile.write(zfobj.read(name))
+            outfile.close()
+    return mainfolder
+
+
+if 'test' in sys.argv:
+    os.chdir("dist")
+    rmrf("library")
+    mainfolder = unzip('library.zip', 'library')
+
+    print os.path.realpath("../../libs/OpenGL")
+    unzip("../../libs/OpenGL.zip", "library")
+
+    surse = os.listdir("..")    
+    for f in surse:
+        if f.endswith(".py"):
+            pyc = os.path.join("library", f + "c")
+            if os.path.isfile(pyc):
+                print pyc
+                os.remove(pyc)
+    sys.exit(0)
+    
+
 #mfcdir = 'C:/WINDOWS/WinSxS/x86_Microsoft.VC90.MFC_1fc8b3b9a1e18e3b_9.0.30729.4148_x-ww_a57c1f53'
 
 #mfcfiles = [join(mfcdir, i) for i in ["mfc90.dll",  "mfc90u.dll",  "mfcm90.dll",  "mfcm90u.dll",  "Microsoft.VC90.MFC.manifest"]]
